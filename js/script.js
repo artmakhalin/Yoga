1698ef6f92360074a764edcacd67b77f75513494
artmakhalin <artmakhalin@gmail.com>
2025-12-22 11:45:17 -0500

    timer added

--- js/script.js
+++ js/script.js
@@ -32,4 +32,51 @@ window.addEventListener("DOMContentLoaded", () => {
             }
         }
     });
+
+    //Timer
+
+    let deadline = "2025-12-25";
+
+    function getTimeRemaining(endTime) {
+        let t = Date.parse(endTime) - Date.parse(new Date());
+        let seconds = Math.floor((t / 1000) % 60);
+        let minutes = Math.floor((t / 1000 / 60) % 60);
+        let hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            "total": t,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds
+        };
+    } 
+
+    function setClock(id, endTime) {
+        let timer = document.getElementById(id);
+        let hours = timer.querySelector(".hours");
+        let minutes = timer.querySelector(".minutes");
+        let seconds = timer.querySelector(".seconds");
+        let timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endTime);
+
+            function addZero(num) {
+                return num <= 9 ? "0" + num : num;
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+            }
+        }
+    }
+
+    setClock("timer", deadline);
 });
