e571805a95f33c941ee415d8307fb0cb161cd7d1
artmakhalin <artmakhalin@gmail.com>
2025-12-22 13:37:23 -0500

    modal window added

--- js/script.js
+++ js/script.js
@@ -1,82 +1,107 @@
 window.addEventListener("DOMContentLoaded", () => {
-    'use strict';
-    let tab = document.querySelectorAll(".info-header-tab"),
+  "use strict";
+  let tab = document.querySelectorAll(".info-header-tab"),
     info = document.querySelector(".info-header"),
     tabContent = document.querySelectorAll(".info-tabcontent");
 
-    function hideTabContent(a) {
-        for (let i = a; i < tabContent.length; i++) {
-            tabContent[i].classList.remove("show");
-            tabContent[i].classList.add("hide");
-        }
+  function hideTabContent(a) {
+    for (let i = a; i < tabContent.length; i++) {
+      tabContent[i].classList.remove("show");
+      tabContent[i].classList.add("hide");
     }
+  }
 
-    hideTabContent(1);
+  hideTabContent(1);
 
-    function showTabContent(b) {
-        if (tabContent[b].classList.contains("hide")) {
-            tabContent[b].classList.remove("hide");
-            tabContent[b].classList.add("show");
-        }
+  function showTabContent(b) {
+    if (tabContent[b].classList.contains("hide")) {
+      tabContent[b].classList.remove("hide");
+      tabContent[b].classList.add("show");
     }
+  }
 
-    info.addEventListener("click", (event) => {
-        let target = event.target;
-        if (target && target.classList.contains("info-header-tab")) {
-            for (let i = 0; i < tab.length; i++) {
-                if (target == tab[i]) {
-                    hideTabContent(0);
-                    showTabContent(i);
-                    break;
-                }
-            }
+  info.addEventListener("click", (event) => {
+    let target = event.target;
+    if (target && target.classList.contains("info-header-tab")) {
+      for (let i = 0; i < tab.length; i++) {
+        if (target == tab[i]) {
+          hideTabContent(0);
+          showTabContent(i);
+          break;
         }
-    });
-
-    //Timer
-
-    let deadline = "2025-12-25";
-
-    function getTimeRemaining(endTime) {
-        let t = Date.parse(endTime) - Date.parse(new Date());
-        let seconds = Math.floor((t / 1000) % 60);
-        let minutes = Math.floor((t / 1000 / 60) % 60);
-        let hours = Math.floor((t / (1000 * 60 * 60)));
-
-        return {
-            "total": t,
-            "hours": hours,
-            "minutes": minutes,
-            "seconds": seconds
-        };
-    } 
-
-    function setClock(id, endTime) {
-        let timer = document.getElementById(id);
-        let hours = timer.querySelector(".hours");
-        let minutes = timer.querySelector(".minutes");
-        let seconds = timer.querySelector(".seconds");
-        let timeInterval = setInterval(updateClock, 1000);
-
-        function updateClock() {
-            let t = getTimeRemaining(endTime);
-
-            function addZero(num) {
-                return num <= 9 ? "0" + num : num;
-            }
-
-            hours.textContent = addZero(t.hours);
-            minutes.textContent = addZero(t.minutes);
-            seconds.textContent = addZero(t.seconds);
-
-            if (t.total <= 0) {
-                clearInterval(timeInterval);
-                hours.textContent = "00";
-                minutes.textContent = "00";
-                seconds.textContent = "00";
-            }
-        }
+      }
     }
+  });
 
-    setClock("timer", deadline);
+  //Timer
+
+  let deadline = "2025-12-25";
+
+  function getTimeRemaining(endTime) {
+    let t = Date.parse(endTime) - Date.parse(new Date());
+    let seconds = Math.floor((t / 1000) % 60);
+    let minutes = Math.floor((t / 1000 / 60) % 60);
+    let hours = Math.floor(t / (1000 * 60 * 60));
+
+    return {
+      total: t,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function setClock(id, endTime) {
+    let timer = document.getElementById(id);
+    let hours = timer.querySelector(".hours");
+    let minutes = timer.querySelector(".minutes");
+    let seconds = timer.querySelector(".seconds");
+    let timeInterval = setInterval(updateClock, 1000);
+
+    function updateClock() {
+      let t = getTimeRemaining(endTime);
+
+      function addZero(num) {
+        return num <= 9 ? "0" + num : num;
+      }
+
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        hours.textContent = "00";
+        minutes.textContent = "00";
+        seconds.textContent = "00";
+      }
+    }
+  }
+
+  setClock("timer", deadline);
+
+  //Modal
+
+  let more = document.querySelector(".more");
+  let overlay = document.querySelector(".overlay");
+  let close = document.querySelector(".popup-close");
+  let descriptionBtns = document.querySelectorAll(".description-btn");
+
+  more.addEventListener("click", showModalWindow);
+
+  close.addEventListener("click", hideModalWindow);
+
+  descriptionBtns.forEach(btn => btn.addEventListener("click", showModalWindow));
+
+  function showModalWindow() {
+    overlay.style.display = "block";
+    this.classList.add("more-splash");
+    document.body.style.overflow = "hidden";
+  }
+
+  function hideModalWindow() {
+    overlay.style.display = "none";
+    more.classList.remove("more-splash");
+    document.body.style.overflow = "";
+  }
 });
